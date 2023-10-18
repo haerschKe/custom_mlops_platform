639b6025bde9da00fb238c5aedc4e834f3d865ed
haerschKe <kevin.hirschmann1994@gmail.com>
2023-10-18 23:29:17 +0200

    add airflow compose, docker network for both compose files, working mlflow dag

--- README.md
+++ README.md
@@ -9,3 +9,24 @@ MLOPS Platform:
 MLFlow + Airflow + Prometheus/Grafana
 
 Each component has a separate subfolder with its code and configuration. In **example_model_dvc** is an example of an SGD Classifier with Imagenette160 dataset and DVC handling (with local remote storage).
+
+There is an example DAG which logs into the MLFlow under an "Demo" Experiment.
+
+The MLFow and Airflow Containers are in the same docker network, although they are configured in two different docker-compose files (just for better structuring).
+The network is created in the MLFlow compose file, while the airflow compose file refers to the created network.
+
+There is an custom Airflow Dockerfile which is used for the compose stuff. The Dockerfile only adds the requirements.txt for the airflow instance.
+
+## How to run:
+Go to the project root directory.
+
+Start MLFow: 
+docker-compose -f docker-compose.yaml up -d
+
+Start Airflow: docker-compose -f airflow_docker/docker-compose-airflow.yaml up -d
+
+## Credentials
+Airflow Login: User "airflow", PW "airflow" --> default values of Airflow Docker Config
+
+## TODO:
+- Grafana/Prometheus
---
+++ airflow_docker/dags/mlflow_dag.py
@@ -0,0 +1,80 @@
+from datetime import timedelta
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
+
+from airflow.utils.dates import days_ago
+
+
+def test_mlflow():
+    import mlflow
+    mlflow.set_tracking_uri("http://mlflow:5000")
+    mlflow.set_experiment('demo')
+
+
+    with mlflow.start_run():
+        mlflow.log_param("x", 1)
+    mlflow.end_run()
+    return "Test"
+
+
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'sla_miss_callback': yet_another_function,
+    # 'trigger_rule': 'all_success'
+}
+dag = DAG(
+    'MLFlow_Test_local',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(2),
+    tags=['example'],
+)
+
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag,
+)
+
+
+t2 = PythonOperator(
+    task_id='mlflow',
+    python_callable=test_mlflow,
+    dag=dag
+)
+
+# t2 = PythonVirtualenvOperator(
+#     task_id='mlflow_test',
+#     python_callable=test_mlflow,
+#     dag=dag,
+#     requirements="mlflow",
+#     )
+
+
+t1 >> t2
